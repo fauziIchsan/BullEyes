fe69aef4041b712d2c3bd8191cab9c83f9e65cc3
fauzi_ichsan <ichsan_196@icloud.com>
2017-11-08 15:17:10 +0700

    Initial Commit

---
+++ BullEyes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BullEyes
+//
+//  Created by Achmad Ichsan Fauzi on 08/11/17.
+//  Copyright © 2017 Ichsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
